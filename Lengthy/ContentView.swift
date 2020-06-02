2af4e23208cd13ad98b0af3f257176223598fb03
AF3 <ethanfarahmand@ymail.com>
2020-06-02 16:41:26 -0400

    added UI and Logic

--- Lengthy/ContentView.swift
+++ Lengthy/ContentView.swift
@@ -9,8 +9,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inp = ""
+    @State var from = 0
+    @State var to = 4
+    let units = ["meters","kilometers","feet","yards","miles"]
+    var inMeasure: Measurement<UnitLength>{
+        switch from {
+        case 0:
+            return Measurement(value: Double(inp) ?? 0, unit: .meters)
+        case 1:
+            return Measurement(value: Double(inp) ?? 0, unit: .kilometers)
+        case 2:
+            return Measurement(value: Double(inp) ?? 0, unit: .feet)
+        case 3:
+            return Measurement(value: Double(inp) ?? 0, unit: .yards)
+        case 4:
+            return Measurement(value: Double(inp) ?? 0, unit: .miles)
+        default:
+            return Measurement(value: 0, unit: .miles)
+        }
+    }
+    var outMeasure: Measurement<UnitLength>{
+        switch to {
+        case 0:
+            return inMeasure.converted(to: .meters)
+        case 1:
+            return inMeasure.converted(to: .kilometers)
+        case 2:
+            return inMeasure.converted(to: .feet)
+        case 3:
+            return inMeasure.converted(to: .yards)
+        case 4:
+            return inMeasure.converted(to: .miles)
+        default:
+            return inMeasure
+        }
+    }
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form{
+                Section(header: Text("Convert From:")){
+                    TextField("From:", text: $inp)
+                        .keyboardType(.decimalPad)
+                    Picker("Unit", selection: $from, content: {
+                        ForEach(0..<5){
+                            Text("\(self.units[$0])")
+                        }
+                    }).pickerStyle(SegmentedPickerStyle())
+                }
+                Section(header: Text("Convert To:")){
+                    Picker("Unit", selection: $to, content: {
+                        ForEach(0..<5){
+                            Text("\(self.units[$0])")
+                        }
+                    }).pickerStyle(SegmentedPickerStyle())
+                    Text("un2: \(outMeasure.value)")
+                }
+            }
+        .navigationBarTitle("Lenghty Convert")
+        }
     }
 }
 
